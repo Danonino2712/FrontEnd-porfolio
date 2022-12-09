3d2fdb01f9ef8baed9f55c5834c9403231417619
Danonino2712 <agustinadaniela12015@gmail.com>
2022-12-09 16:22:20 -0300

    agregando componentes y pasando de maqueta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    HeaderComponent,
+    FooterComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SobreMiComponent,
+    LoginComponent,
+    ProyectosComponent,
+    SkillsComponent,
   ],
   imports: [
     BrowserModule,
